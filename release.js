5ecfeb08b502952e7d152fa016572b11acfc8f5a
John IV (MAME, MAME32, MAMEUI, IV/Play) <john-iv@users.noreply.github.com>
2026-08-31 23:16:03 -0700

    Add files via upload

--- release.js
+++ release.js
@@ -2,8 +2,8 @@
    Bump per release; buildId is the field that drives the update check. */
 var IVPLAY = {
     version:    "2.8.12",
-    buildId:    "afa430c3-b571-4f16-b8a5-ea329ce69141",
-    date:       "10:45 PM 8/12/2026",
+    buildId:    "54442eb7-ff78-420b-a164-9acc898d6c83",
+    date:       "11:13 PM 8/31/2026",
     url:        "https://github.com/john-iv/iv-play/releases",
     artMameVer: "0.289",
     artDate:    "5:54 PM 7/30/2026",
